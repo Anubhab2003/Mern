e917585c2f9b25967b21040b545e73072d353363
Anubhab2003 <anubhabchowdhury01@gmail.com>
2024-06-12 00:47:35 +0530

    loop in array

--- business generator/index.js
+++ business generator/index.js
@@ -7,3 +7,12 @@ n.unshift(45)
 console.log(typeof n)
 console.log(n)
 console.log(a.concat(n))
+console.log(a.sort())
+//NEXT
+t=[1,2,3,4,5,6]
+function product(a,b){
+    return a*b
+}
+console.log(t.reduce(product))
+let l=Array.from("Anubhab")
+console.log(l)
