acedd293a6c4193c39cdffeb6a166b7f192e6a06
Anubhab2003 <anubhabchowdhury01@gmail.com>
2024-06-28 22:58:23 +0530

    work

---
+++ Advance Javascript/script.js
@@ -0,0 +1,17 @@
+async function sleep(){
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            resolve(45)
+        },1200);
+    })
+}
+let a=await sleep()
+let b=await sleep()
+// IIFE-IMMEDIATE INVOKE FUNCTION
+(async function main(){
+    let a=await sleep();
+    console.log(a)
+    let b=await sleep();
+    console.log(b)
+
+})()
