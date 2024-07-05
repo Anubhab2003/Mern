62e0d426ba2bbca82d4f1b6daf96619712f2015a
Anubhab2003 <anubhabchowdhury01@gmail.com>
2024-07-05 22:21:39 +0530

    jsx

--- React 1/textutils/src/App.js
+++ React 1/textutils/src/App.js
@@ -1,24 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
-
+let name="Anubhab"
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <nav>
+      <li>Home</li>
+      <li>Services</li>
+      <li>Contact us</li>
+    </nav>
+    <div className="container">
+      <h1>Hello {name}</h1>
+      <h1>Your crush</h1>
+      {/* <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTXmJ7mBglkZFco-MTcs9D5EWsoGpA1hOE9Ug&s" alt="" width={4500} height={4500}/> */}
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Est eaque soluta veniam excepturi eos et consequuntur quis ab inventore, a dolore cumque sapiente dicta rerum, quae qui? Iste aliquid temporibus dignissimos harum.
     </div>
+    </>
+    
+
   );
 }
 
