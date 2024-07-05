d8ed757e18f26a3293f541af94ea99f0c88fc68e
Anubhab2003 <anubhabchowdhury01@gmail.com>
2024-07-05 23:31:51 +0530

    simple navbar website

--- React 1/textutils/src/App.js
+++ React 1/textutils/src/App.js
@@ -4,17 +4,42 @@ let name="Anubhab"
 function App() {
   return (
     <>
-    <nav>
-      <li>Home</li>
-      <li>Services</li>
-      <li>Contact us</li>
+        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="/">TextUtil</a>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="/">Home</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/">About</a>
+            </li>
+            {/* <li className="nav-item dropdown">
+              <a className="nav-link dropdown-toggle" href="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Dropdown
+              </a>
+              <ul className="dropdown-menu">
+                <li><a className="dropdown-item" href="/">Action</a></li>
+                <li><a className="dropdown-item" href="/">Another action</a></li>
+                <li><hr className="dropdown-divider"/></li>
+                <li><a className="dropdown-item" href="/">Something else here</a></li>
+              </ul>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link disabled" aria-disabled="true">Disabled</a>
+            </li> */}
+          </ul>
+          <form className="d-flex" role="search">
+            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+            <button className="btn btn-outline-success" type="submit">Search</button>
+          </form>
+        </div>
+      </div>
     </nav>
-    <div className="container">
-      <h1>Hello {name}</h1>
-      <h1>Your crush</h1>
-      {/* <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTXmJ7mBglkZFco-MTcs9D5EWsoGpA1hOE9Ug&s" alt="" width={4500} height={4500}/> */}
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Est eaque soluta veniam excepturi eos et consequuntur quis ab inventore, a dolore cumque sapiente dicta rerum, quae qui? Iste aliquid temporibus dignissimos harum.
-    </div>
     </>
     
 
