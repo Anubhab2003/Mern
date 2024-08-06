a5a7765b863b7c868950b93126b5223569d28a63
Anubhab2003 <anubhabchowdhury01@gmail.com>
2024-08-06 13:27:39 +0530

    updated feature

--- React 1/textutils/src/Components/TextForm.js
+++ React 1/textutils/src/Components/TextForm.js
@@ -6,6 +6,11 @@ export default function TextForm(props) {
     let newText=text.toUpperCase();
     setText(newText);
   };
+  const handleLoClick = () => {
+    // console.log("Uppercase was clicked"+text);
+    let newText=text.toLowerCase();
+    setText(newText);
+  };
 
   const handleOnChange = (event) => {
     // console.log("On change");
@@ -16,7 +21,8 @@ export default function TextForm(props) {
   const [text, setText] = useState("");
 
   return (
-    <div>
+    <>
+    <div className="container">
       <h1>{props.heading}</h1>
       <div className="mb-3">
         <textarea
@@ -30,6 +36,19 @@ export default function TextForm(props) {
       <button className="btn btn-primary" onClick={handleUpClick}>
         Convert to Uppercase
       </button>
+      <button className="btn btn-primary mx-3" onClick={handleLoClick}>
+        Convert to Lowercase
+      </button>
     </div>
+    <div className="container my-3">
+      <h1>Your Text Summary</h1>
+      <p>{text.split(" ").length} Words and {text.length} characters</p>
+      
+      <p>{0.008 * text.split(" ").length} Minutes read </p>
+      <h2>Preview</h2>
+      <p>{text}</p>
+
+    </div>
+    </>
   );
 }
