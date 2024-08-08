adc603ffe75eb75c209b2a434226c8f04fa8a8a6
Anubhab2003 <anubhabchowdhury01@gmail.com>
2024-08-08 20:14:50 +0530

    all

--- React 1/textutils/src/App.js
+++ React 1/textutils/src/App.js
@@ -3,11 +3,13 @@ import Navbar from "./Components/Navbar";
 import "./App.css";
 import TextForm from "./Components/TextForm";
 import About from "./Components/About";
+import { useState } from "react";
 let name = "Anubhab";
 function App() {
+  const [mode, setMode] = useState('dark');//wheather dark mode is enable or not
   return (
     <>
-      <Navbar title="TextUtils" aboutText="About TextUtil" />
+      <Navbar title="TextUtils" aboutText="About TextUtil" mode={mode}/>
       <div className="container my-3">
         <TextForm heading="Enter text to analyse below"/>
       </div>
