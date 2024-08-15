830c0011953a940402c0559fc603b97424a34f83
Anubhab2003 <anubhabchowdhury01@gmail.com>
2024-08-15 16:58:32 +0530

    bug fixed

--- React 1/textutils/src/Components/TextForm.js
+++ React 1/textutils/src/Components/TextForm.js
@@ -71,7 +71,7 @@ export default function TextForm(props) {
     </div>
     <div className="container my-3" style={{color:props.mode==='dark'?'white':'#042743'}}>
       <h2>Your Text Summary</h2>
-      <p>{text.split(" ").length} Words and {text.length} characters</p>
+      <p>{text.split(" ").length-1} Words and {text.length} characters</p>
       
       <p>{0.008 * text.split(" ").length} Minutes read </p>
       <h2>Preview</h2>
